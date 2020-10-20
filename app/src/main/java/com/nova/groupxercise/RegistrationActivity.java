549221953af771bac54aaf2b4f158621368bdbcf
Ethan Padden <ethanpadden@gmail.com>
2020-10-20 17:54:56 +0100

    Add check that all fields are filled before registering

--- app/src/main/java/com/nova/groupxercise/RegistrationActivity.java
+++ app/src/main/java/com/nova/groupxercise/RegistrationActivity.java
@@ -3,12 +3,39 @@ package com.nova.groupxercise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
+    private Button m_registrationBtn;
+    private EditText m_emailEt;
+    private EditText m_passwordEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        // Initialise components
+        m_registrationBtn = findViewById(R.id.btn_register);
+        m_emailEt = findViewById(R.id.et_email);
+        m_passwordEt = findViewById(R.id.et_password);
+
+        // Set event listeners
+        m_registrationBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String email = m_emailEt.getText().toString();
+                String password = m_passwordEt.getText().toString();
+
+                if (email.compareTo("") == 0 || password.compareTo("") == 0) {
+                    Toast.makeText(RegistrationActivity.this, "Enter all details", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(RegistrationActivity.this, "Registering...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
 }
