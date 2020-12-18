33d623df20016b2c3b0ea7b5ec2f4ffe979d5d43
Ethan Padden <ethanpadden@gmail.com>
2020-12-18 22:37:34 +0000

    Add method to register user. User is registered if the email and password are valid.

--- app/src/main/java/com/nova/groupxercise/MainActivity.java
+++ app/src/main/java/com/nova/groupxercise/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -16,9 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser == null) {
+        if (currentUser == null) {
             Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
             startActivity(intent);
+        } else {
+            String msg = "User signed in: " + currentUser.getEmail();
+            Toast.makeText(MainActivity.this, msg,
+                    Toast.LENGTH_SHORT).show();
         }
     }
 
--- app/src/main/java/com/nova/groupxercise/RegistrationActivity.java
+++ app/src/main/java/com/nova/groupxercise/RegistrationActivity.java
@@ -1,5 +1,6 @@
 package com.nova.groupxercise;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,7 +10,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.auth.FirebaseUser;
 
 public class RegistrationActivity extends AppCompatActivity {
@@ -37,18 +42,46 @@ public class RegistrationActivity extends AppCompatActivity {
                 String email = m_emailEt.getText().toString();
                 String password = m_passwordEt.getText().toString();
 
-                validateFields(email, password);
+                boolean fieldsAreValid = validateFields(email, password);
+                if(fieldsAreValid) registerUser(email, password);
             }
         });
     }
 
-    private void validateFields(String email, String password) {
+    private boolean validateFields(String email, String password) {
+        boolean fieldsAreValid = false;
         if (email.equals("") || password.equals("")) {
             Toast.makeText(RegistrationActivity.this, "Enter all details", Toast.LENGTH_SHORT).show();
         } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             Toast.makeText(RegistrationActivity.this, "Please enter a valid email", Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(RegistrationActivity.this, "Registering...", Toast.LENGTH_SHORT).show();
+            fieldsAreValid = true;
         }
+        return fieldsAreValid;
+    }
+
+    private void registerUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            String successMsg = "Success";
+                            String dn = user.getDisplayName();
+                            Toast.makeText(RegistrationActivity.this, successMsg,
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+
+                            String errorMsg = "Authentication failed";
+                            if(task.getException() instanceof FirebaseAuthWeakPasswordException)
+                                errorMsg = task.getException().getMessage();
+
+                            Toast.makeText(RegistrationActivity.this, errorMsg,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
     }
 }
