587540befded75ed0e6891b037e08d176b20fdec
Ethan Padden <ethanpadden@gmail.com>
2021-01-24 19:48:16 +0000

    Edit fragment UI structure

--- app/src/main/java/com/nova/groupxercise/ExerciseListItemFragment.java
+++ app/src/main/java/com/nova/groupxercise/ExerciseListItemFragment.java
@@ -11,6 +11,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.joda.time.DateTime;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -31,6 +39,9 @@ public class ExerciseListItemFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
 
     private TextView mSetGoalTitleText;
+    private TextView mSuggestedGoalText;
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+
 
     public ExerciseListItemFragment() {
         // Required empty public constructor
@@ -64,7 +75,52 @@ public class ExerciseListItemFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         mSetGoalTitleText =  view.findViewById( R.id.text_set_goal_title );
+        mSuggestedGoalText =  view.findViewById( R.id.text_suggested_goal );
         mSetGoalTitleText.setText( "Set Goal: " + mExerciseName );
+    }
+
+    public void calculateStrengthGoal( String exerciseName) {
+        User user = User.getInstance();
+        mSuggestedGoalText.setText( "Loading..." );
+        // Check if all user details are set correctly
+        User testUser = new User();
+
+        if ( !user.detailsAreValid() ) {
+            // TODO: replace with returning null with toast message
+            testUser.setName( "John Doe" );
+            testUser.setSex( User.Sex.MALE );
+            testUser.setWeight( 68 );
+            testUser.setDob( new DateTime( 1990, 9,1,0,0 ) );
+        }
+
+        int weightClass = getWeightClass( testUser.getWeight() );
+        String path = "strength_standards/" + exerciseName + "/" + testUser.getSex().toString() + "/" + weightClass;
+        DatabaseReference childRef = mRootRef.child( path );
+        final Goal goal = new Goal( exerciseName );
+        childRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange( DataSnapshot dataSnapshot) {
+                int[] standards = new int[5];
+                Long[] standardLongValues = new Long[5];
+                standardLongValues[0] = (Long) dataSnapshot.child( "Beginner" ).getValue();
+                standardLongValues[1] = (Long) dataSnapshot.child( "Novice" ).getValue();
+                standardLongValues[2] = (Long) dataSnapshot.child( "Intermediate" ).getValue();
+                standardLongValues[3] = (Long) dataSnapshot.child( "Advanced" ).getValue();
+                standardLongValues[4] = (Long) dataSnapshot.child( "Elite" ).getValue();
+                for( int i = 0; i < standardLongValues.length; i++ ) {
+                    standards[i] = standardLongValues[i] == null ? null : Math.toIntExact(standardLongValues[i]);
+                }
+
+                goal.setmStandards( standards );
+                mSuggestedGoalText.setText( goal.toString() );
+            }
+
+            @Override
+            public void onCancelled( DatabaseError databaseError) {}
+        });
+    }
+    private int getWeightClass(float weight) {
+        return (int)(Math.floor( weight/5 )*5);
     }
 
     @Override
