92fdc3ffcb2a32a30efd403991d7ffa7c01e0227
Ethan Padden <ethanpadden@gmail.com>
2021-01-25 15:54:47 +0000

    Create fragments for home (temporary), discoveries and goal screens

---
+++ app/src/main/java/com/nova/groupxercise/GoalsFragment.java
@@ -0,0 +1,20 @@
+package com.nova.groupxercise;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class GoalsFragment extends Fragment {
+    @Override
+    public View onCreateView( LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate( R.layout.fragment_goals, container, false );
+    }
+
+
+}
