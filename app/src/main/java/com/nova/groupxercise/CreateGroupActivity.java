cc06c2b0c2919e68e26511eab4590883be592ff4
Ethan Padden <ethanpadden@gmail.com>
2021-02-04 17:59:55 +0000

    Add button to link to create group activity

---
+++ app/src/main/java/com/nova/groupxercise/CreateGroupActivity.java
@@ -0,0 +1,14 @@
+package com.nova.groupxercise;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_create_group );
+    }
+}
--- app/src/main/java/com/nova/groupxercise/GroupsFragment.java
+++ app/src/main/java/com/nova/groupxercise/GroupsFragment.java
@@ -1,14 +1,19 @@
 package com.nova.groupxercise;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
 public class GroupsFragment extends Fragment {
+    private Button mCreateGroupBtn;
 
     @Override
     public void onCreate( Bundle savedInstanceState ) {
@@ -21,6 +26,22 @@ public class GroupsFragment extends Fragment {
                               Bundle savedInstanceState ) {
         // Inflate the layout for this fragment
         return inflater.inflate( R.layout.fragment_groups, container, false );
+
+
     }
 
+    @Override
+    public void onViewCreated( @NonNull View view, @Nullable Bundle savedInstanceState ) {
+        super.onViewCreated( view, savedInstanceState );
+
+        mCreateGroupBtn = view.findViewById( R.id.btn_create_group );
+
+        mCreateGroupBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View view ) {
+                Intent intent = new Intent( getActivity(), CreateGroupActivity.class );
+                startActivity( intent );
+            }
+        } );
+    }
 }
