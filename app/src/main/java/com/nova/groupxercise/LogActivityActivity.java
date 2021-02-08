0317d0d0a2fe612bc6dde306d7fc9956f8b6c822
Ethan Padden <ethanpadden@gmail.com>
2021-02-08 14:04:25 +0000

    Add method to retrieve group goals for log activity screen

--- app/src/main/java/com/nova/groupxercise/LogActivityActivity.java
+++ app/src/main/java/com/nova/groupxercise/LogActivityActivity.java
@@ -27,6 +27,7 @@ public class LogActivityActivity extends AppCompatActivity {
     private GoalItemsAdapter mItemsAdapter;
     private TextView mLoadingText;
     private ListView mListView;
+    private ArrayList<Group> mGroups;
 
 
     @Override
@@ -49,9 +50,116 @@ public class LogActivityActivity extends AppCompatActivity {
         } );
 
         retrieveGoals();
+        retrieveGroupIds();
     }
 
+    /**
+     * Retrieves the group IDs of the groups that the user is a member of from the DB
+     */
+    private void retrieveGroupIds() {
+        // Create empty list for the group IDs
+        final ArrayList<String> groupIds = new ArrayList<>(  );
 
+        // Get the current user ID
+        String currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        // Path to the reference
+        String usersGroupPath = "user_groups/" + currentUserId;
+        // Get the DBr reference
+        DatabaseReference childRef = mRootRef.child( usersGroupPath );
+
+        childRef.addListenerForSingleValueEvent( new ValueEventListener() {
+            @Override
+            public void onDataChange( DataSnapshot dataSnapshot ) {
+                for ( DataSnapshot usersGroupsDataSnapshot : dataSnapshot.getChildren() ) {
+                    groupIds.add(  usersGroupsDataSnapshot.getKey());
+                }
+
+                retrieveGroupGoals(groupIds);
+            }
+
+            @Override
+            public void onCancelled( DatabaseError databaseError ) {
+            }
+        } );
+    }
+
+    /**
+     * Given a list of group IDs, retrieve the group goals from the DB
+     * @param groupIds the list of group IDs
+     */
+    private void retrieveGroupGoals( ArrayList<String> groupIds) {
+        // Create an empty list for the groups
+        mGroups = new ArrayList<>();
+
+        // The UI is updated when all of the group names have been added
+        // Necessary because of the async call within the for loop
+        final int expectedSize = groupIds.size();
+
+        for( final String groupId : groupIds) {
+            String groupPath = "groups/" + groupId;
+            // Get the DBr reference
+            DatabaseReference groupRef = mRootRef.child( groupPath );
+
+            groupRef.addListenerForSingleValueEvent( new ValueEventListener() {
+                @Override
+                public void onDataChange( DataSnapshot dataSnapshot ) {
+                    String groupName = dataSnapshot.child( "name" ).getValue().toString();
+
+                    Group group = new Group( groupName, groupId );
+                    DataSnapshot groupGoalsDataSnapshot = dataSnapshot.child( "goals" );
+
+                    // If the group has goals
+                    if(groupGoalsDataSnapshot.exists()) {
+                        ArrayList<Goal> groupGoals = new ArrayList<>(  );
+
+                        for(DataSnapshot groupGoalDataSnapshot : groupGoalsDataSnapshot.getChildren()){
+                            String exerciseName = groupGoalDataSnapshot.getKey();
+                            String targetStr = groupGoalDataSnapshot.getValue().toString();
+                            float target = Float.parseFloat( targetStr );
+                            Goal goal = new Goal( exerciseName , 0, target );
+                            groupGoals.add( goal );
+                        }
+
+                        group.setGoals( groupGoals );
+                    }
+
+                    mGroups.add( group );
+
+
+                    if(mGroups.size() == expectedSize) {
+                        displayGroupGoals();
+                    }
+                }
+
+                @Override
+                public void onCancelled( DatabaseError databaseError ) {
+                }
+            } );
+        }
+        if(mGroups.size() == 0) {
+            mLoadingText.setText( "You have no groups" );
+        }
+
+    }
+
+    /**
+     * Builds a list for every group to display the group goals
+     */
+    private void displayGroupGoals() {
+        for(Group group : mGroups) {
+
+            if(group.getGoals() != null) {
+                for(Goal goal: group.getGoals()) {
+                    mGoalsList.add( goal );
+                }
+            }
+        }
+
+        // Update UI
+        mLoadingText.setVisibility( View.GONE );
+        mListView.setAdapter( mItemsAdapter );
+    }
 
     /**
      * Gets the list of goals from the DB and makes the UI list visible when retrieved
@@ -96,10 +204,6 @@ public class LogActivityActivity extends AppCompatActivity {
                     // Add the goal to the list
                     mGoalsList.add( new Goal( exerciseName, currentStatus, target ) );
                 }
-
-                // Update UI
-                mLoadingText.setVisibility( View.GONE );
-                mListView.setAdapter( mItemsAdapter );
             }
 
             @Override
