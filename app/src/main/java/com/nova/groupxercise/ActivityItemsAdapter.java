90615cdbec6e67bb3a12d61f0eb490c408fd03af
Ethan Padden <ethanpadden@gmail.com>
2021-02-08 17:55:19 +0000

    Add untracked files

---
+++ app/src/main/java/com/nova/groupxercise/ActivityItemsAdapter.java
@@ -0,0 +1,39 @@
+package com.nova.groupxercise;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ActivityItemsAdapter extends ArrayAdapter {
+    private static final String LOG_TAG = GroupItemsAdapter.class.getSimpleName();
+
+    public ActivityItemsAdapter( Activity context, ArrayList< Group > groups ) {
+        super( context, 0, groups );
+    }
+
+    @Override
+    public View getView( int position, View convertView, ViewGroup parent ) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if ( listItemView == null ) {
+            listItemView = LayoutInflater.from( getContext() ).inflate(
+                    R.layout.layout_activity_list_item, parent, false );
+        }
+
+        ExerciseActivity currentActivity = ( ExerciseActivity ) getItem( position );
+
+        TextView exerciseNameText = listItemView.findViewById( R.id.activity_exercise_name );
+        exerciseNameText.setText( currentActivity.getmExerciseName() );
+        TextView levelText = listItemView.findViewById( R.id.activity_level );
+        levelText.setText( Float.toString( currentActivity.getmLevel() ));
+        TextView timeText = listItemView.findViewById( R.id.activity_time );
+        timeText.setText( currentActivity.getmTime().toString());
+
+        return listItemView;
+    }
+}
