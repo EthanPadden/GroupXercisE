42db709b10de526457c3979a9544fd79d3ecf989
Ethan Padden <ethanpadden@gmail.com>
2022-11-02 09:45:47 +0000

    Load images on each walking plan fragment

--- app/src/main/java/com/nova/groupxercise/Fragments/WalkingPlanListItemFragment.java
+++ app/src/main/java/com/nova/groupxercise/Fragments/WalkingPlanListItemFragment.java
@@ -4,13 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.nova.groupxercise.R;
+import com.squareup.picasso.Picasso;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +32,7 @@ public class WalkingPlanListItemFragment extends Fragment {
     private String mWalkingPlanName;
 
     private TextView mSetGoalTitleText;
+    ImageView mImage;
 
     public WalkingPlanListItemFragment() {
         // Required empty public constructor
@@ -69,5 +78,60 @@ public class WalkingPlanListItemFragment extends Fragment {
         // Set the fragment title
         mSetGoalTitleText.setText( getResources().getString( R.string.set_goal_title ) + " " + mWalkingPlanName );
 
+        // Set the walking plan image
+        // getting ImageView by its id
+        mImage = view.findViewById(R.id.img_walking_plan);
+
+        // we will get the default FirebaseDatabase instance
+        FirebaseDatabase firebaseDatabase
+                = FirebaseDatabase.getInstance();
+
+        // we will get a DatabaseReference for the database
+        // root node
+        DatabaseReference databaseReference
+                = firebaseDatabase.getReference();
+
+        // Here "image" is the child node value we are
+        // getting child node data in the getImage variable
+        String path = "exercise_list/Cardio/Walking/" + mWalkingPlanName + "/image";
+        DatabaseReference getImage
+                = databaseReference.child(path);
+
+        // Adding listener for a single change
+        // in the data at this location.
+        // this listener will triggered once
+        // with the value of the data at the location
+        getImage.addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(
+                            @NonNull DataSnapshot dataSnapshot)
+                    {
+                        // getting a DataSnapshot for the
+                        // location at the specified relative
+                        // path and getting in the link variable
+                        String link = dataSnapshot.getValue(
+                                String.class);
+
+                        // loading that data into rImage
+                        // variable which is ImageView
+                        Picasso.get().load(link).into( mImage );
+                    }
+
+                    // this will called when any problem
+                    // occurs in getting data
+                    @Override
+                    public void onCancelled(
+                            @NonNull DatabaseError databaseError)
+                    {
+                        // we are showing that error message in
+                        // toast
+                        Toast
+                                .makeText(getActivity(),
+                                        "Error Loading Image",
+                                        Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
     }
 }
