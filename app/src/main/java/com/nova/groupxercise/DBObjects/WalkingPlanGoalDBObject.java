fe823918c5d407788a90127690173e08ed0bef16
Ethan Padden <ethanpadden@gmail.com>
2022-11-04 09:34:36 +0000

    Update walking goal setting with last_walk_time set to 0

--- app/src/main/java/com/nova/groupxercise/DBObjects/WalkingPlanGoalDBObject.java
+++ app/src/main/java/com/nova/groupxercise/DBObjects/WalkingPlanGoalDBObject.java
@@ -8,11 +8,13 @@ public class WalkingPlanGoalDBObject {
     public int progress;
     public int todays_step_goal;
     public long start_time;
+    public long last_walk_time;
 
     public WalkingPlanGoalDBObject( String plan, int todays_step_goal ) {
         this.plan = plan;
         this.todays_step_goal = todays_step_goal;
         this.progress = 0;
+        this.last_walk_time = 0;
         this.start_time = DateTime.now().getMillis();
     }
 }
