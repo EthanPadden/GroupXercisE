a0c764a2c836a3a322da557365439e135ab0f251
Ethan Padden <ethanpadden@gmail.com>
2023-03-06 18:15:05 +0000

    Add other images to tutorial

--- app/src/main/java/com/nova/groupxercise/Activities/TutorialActivity.java
+++ app/src/main/java/com/nova/groupxercise/Activities/TutorialActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +14,7 @@ import com.nova.groupxercise.R;
 public class TutorialActivity extends AppCompatActivity {
     private TextView mTutorialText;
     private TextView mTutorialTitleText;
+    private ImageView mTutorialImage;
     private Button mNextButton;
     private int mCurrentStep = 0;
     String[] mTutorialStrings;
@@ -25,9 +27,23 @@ public class TutorialActivity extends AppCompatActivity {
 
         mTutorialText = findViewById(R.id.text_tutorial );
         mTutorialTitleText = findViewById(R.id.text_tutorial_title );
+        mTutorialImage = findViewById(R.id.img_tutorial );
         mNextButton = findViewById(R.id.next_button);
         mTutorialStrings = getResources().getStringArray(R.array.tutorial );
         mTutorialTitleStrings = getResources().getStringArray(R.array.tutorial_titles );
+
+        int[] imageIds = {
+                R.drawable.tutorial_step_0,
+                R.drawable.tutorial_step_1,
+                R.drawable.tutorial_step_1,
+                R.drawable.tutorial_step_3,
+                R.drawable.tutorial_step_4,
+                R.drawable.tutorial_step_5,
+                R.drawable.tutorial_step_5,
+                R.drawable.tutorial_step_7,
+                R.drawable.tutorial_step_8,
+                R.drawable.tutorial_step_9,
+        };
 
         mNextButton.setOnClickListener( new View.OnClickListener() {
             @Override
@@ -36,6 +52,9 @@ public class TutorialActivity extends AppCompatActivity {
                 if ( mCurrentStep < mTutorialStrings.length) {
                     mTutorialText.setText( mTutorialStrings[mCurrentStep]);
                     mTutorialTitleText.setText( mTutorialTitleStrings[mCurrentStep] );
+
+                    mTutorialImage.setImageResource( imageIds[mCurrentStep] );
+
                     // Need to program in images too
                     if ( mCurrentStep == mTutorialStrings.length - 1) {
                         mNextButton.setText("Finish");
