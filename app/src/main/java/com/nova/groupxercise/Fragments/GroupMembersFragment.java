a1cd481ac23939c6ea638c753eb2c301ed32188e
Ethan Padden <ethanpadden@gmail.com>
2023-03-10 09:06:08 +0000

    Use listview rather than linearlayout for group members

--- app/src/main/java/com/nova/groupxercise/Fragments/GroupMembersFragment.java
+++ app/src/main/java/com/nova/groupxercise/Fragments/GroupMembersFragment.java
@@ -4,22 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.nova.groupxercise.Adapters.GroupMembersItemsAdapter;
 import com.nova.groupxercise.Objects.Group;
-import com.nova.groupxercise.Objects.Member;
 import com.nova.groupxercise.R;
 
 public class GroupMembersFragment extends Fragment {
     private Group mGroup;
-    private LinearLayout mGroupMembersLayout;
+    private ListView mGroupMembersListView;
     private boolean mAdminGroup;
     private TextView mGroupMembersLoadingText;
+    private ArrayAdapter mGroupMembersAdapter;
 
     public GroupMembersFragment( Group mGroup, boolean mAdminGroup ) {
         this.mGroup = mGroup;
@@ -37,31 +39,40 @@ public class GroupMembersFragment extends Fragment {
     public void onViewCreated( @NonNull View view, @Nullable Bundle savedInstanceState ) {
         super.onViewCreated( view, savedInstanceState );
 
-        mGroupMembersLayout = view.findViewById( R.id.layout_group_members );
+        mGroupMembersListView = view.findViewById( R.id.list_group_members );
         mGroupMembersLoadingText = view.findViewById( R.id.text_group_members_loading );
+
+        mGroupMembersAdapter = new GroupMembersItemsAdapter( getActivity(), mGroup );
+
 
         // Initially assume the user is not an admin, and add the delete buttons as necessary
 
-        // Add group members to UI
-        for( Member member : mGroup.getmMembers() ) {
-            View memberLayout = LayoutInflater.from( getContext() ).inflate(
-                    R.layout.layout_group_member, null );
-
-            TextView memberNameText = memberLayout.findViewById( R.id.text_member_name );
-            memberNameText.setText( member.getmUsername() );
-
-            if(mGroup.getmCreator().compareTo( member.getmUsername() ) == 0) {
-                TextView adminStatusText = memberLayout.findViewById( R.id.text_member_admin_status );
-                adminStatusText.setVisibility( View.VISIBLE );
-            }
-
-            mGroupMembersLayout.addView( memberLayout );
-        }
+//        // Add group members to UI
+//        for( Member member : mGroup.getmMembers() ) {
+//            View memberLayout = LayoutInflater.from( getContext() ).inflate(
+//                    R.layout.layout_group_member, null );
+//
+//            TextView memberNameText = memberLayout.findViewById( R.id.text_member_name );
+//            memberNameText.setText( member.getmUsername() );
+//
+//            if(mGroup.getmCreator().compareTo( member.getmUsername() ) == 0) {
+//                TextView adminStatusText = memberLayout.findViewById( R.id.text_member_admin_status );
+//                adminStatusText.setVisibility( View.VISIBLE );
+//            }
+//
+//            // Set on click method to open member details fragment
+//            memberLayout.setOnClickListener( new View.OnClickListener() {
+//                public void onClick( View v ) {
+//                    // TODO: show member progress
+//                    Toast.makeText( getActivity(), member.getmUsername(), Toast.LENGTH_SHORT );
+//                }
+//            } );
+//        }
 
         if(mGroup.getmMembers().size() > 0) {
             mGroupMembersLoadingText.setVisibility( View.GONE );
         }
 
-        // TODO: show member progress
+        mGroupMembersListView.setAdapter( mGroupMembersAdapter );
     }
 }
